18becf31927893878a12fd4ae7451d5deffd5cf6
mifodiy911 <mifodiy911@gmail.com>
2024-05-07 18:42:55 +0300

    add Container component

---
+++ src/components/Container/Container.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 0 15px;
+`;
+
+export const FluidContainer = styled(Container)`
+  max-width: 980px;
+`;
