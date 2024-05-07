c2094113e264e24c5d75acf595e064797cc63d37
mifodiy911 <mifodiy911@gmail.com>
2024-05-07 18:54:36 +0300

    fix index.css

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,21 @@
-
+import Growth from "../Growth/Growth";
+import Header from "../Header/Header";
+import MusicCard from "../MusicCard/MusicCard";
+import Benchmark from "../Benchmark/Benchmark";
+import Related from "../Related/Related";
+import Recommend from "../Recommend/Recommend";
 
 function App() {
-
   return (
     <>
-      
+      <Header />
+      <MusicCard />
+      <Growth />
+      <Benchmark />
+      <Related />
+      <Recommend />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
